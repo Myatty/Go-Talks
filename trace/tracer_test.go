602b85908443e28378ecc2044ac47a7f93f17707
Myatty <myintmyatps664@gmail.com>
2025-11-08 21:12:05 +0630

    implement tracer.go

---
+++ trace/tracer_test.go
@@ -0,0 +1,27 @@
+package trace
+
+import (
+	"bytes"
+	"io"
+	"testing"
+)
+
+func New(w io.Writer) Tracer {
+	return &tracer{out: w}
+}
+
+func TestNew(t *testing.T) {
+	var buf bytes.Buffer
+	tracer := New(&buf)
+
+	if tracer == nil {
+		t.Error("Return from New should not be nil!")
+	} else {
+		tracer.Trace("Hello trace package.")
+
+		if buf.String() != "Hello trace package.\n" {
+			t.Errorf("Trace should not write '%s'.", buf.String())
+		}
+	}
+
+}
