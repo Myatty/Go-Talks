2ba741f88236f68fe6935452baadb706a75836ff
Myatty <myintmyatps664@gmail.com>
2025-11-14 22:17:36 +0630

    create cookie to save userinfo backed from OAuth

--- chat/auth.go
+++ chat/auth.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"strings"
+
+	"github.com/stretchr/objx"
 )
 
 type authHandler struct {
@@ -66,6 +69,7 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
+		// Exchange code for token with google
 		code := r.FormValue("code")
 		token, err := GoogleOAuthConfig.Exchange(context.Background(), code)
 		if err != nil {
@@ -73,6 +77,7 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
+		// Fetch user info
 		resp, err := http.Get("https://www.googleapis.com/oauth2/v2/userinfo?access_token=" + token.AccessToken)
 		if err != nil {
 			http.Error(w, "Failed to get user info: "+err.Error(), http.StatusInternalServerError)
@@ -80,9 +85,32 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		defer resp.Body.Close()
 
-		data, _ := io.ReadAll(resp.Body)
-		w.Write(data)
+		// Parse JSON
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			http.Error(w, "Failed reading user info: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
 
+		var userInfo map[string]interface{}
+		if err := json.Unmarshal(body, &userInfo); err != nil {
+			http.Error(w, "Invalid user info: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// Create cookie to save name
+		name := fmt.Sprintf("%v", userInfo["name"])
+		cookieValue := objx.New(map[string]interface{}{
+			"name": name,
+		}).MustBase64()
+
+		http.SetCookie(w, &http.Cookie{
+			Name:  "auth",
+			Value: cookieValue,
+			Path:  "/",
+		})
+
+		http.Redirect(w, r, "/chat", http.StatusTemporaryRedirect)
 	default:
 		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprintf(w, "Auth action %s not supported", action)
